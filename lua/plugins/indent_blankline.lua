245f4a716212c05e34faee5be13350e942ea002b
colo993 <wakula993@gmail.com>
2025-05-11 15:45:23 +0200

    change indentations to mini-indent

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,17 +1,9 @@
 -- lua/plugins/indent_blankline.lua
 
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = function ()
-    require('ibl').setup({
-      indent = { highlight = "IblScope"},
-      scope = {
-        show_exact_scope = false,
-        show_start = false,
-        show_end = false,
-        highlight = "IblScope",
-      }
+  'chasnovski/mini.nvim',
+  config = function()
+    require('mini.indentscope').setup({
     })
   end
 }
