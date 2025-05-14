4bfa813e8937afd247e25ecaf080ed327d657c45
colo993 <wakula993@gmail.com>
2025-05-14 21:55:50 +0200

    add shortcuts to lsp config

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -31,6 +31,6 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set("n", "<leader>nh", ":nohlsearch<CR>")
+vim.keymap.set("n", "<leader>n", ":nohlsearch<CR>")
 vim.wo.number = true
 vim.o.relativenumber = true
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,7 +5,26 @@ return {
 			local lspconfig = require("lspconfig")
 			local on_attach = function(client, bufnr)
 				local opts = { noremap = true, silent = true }
+				-- Go To Definition: Jumps to the primary definition
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+				-- Go To Declaration: Jumps to the declaration (might be different from definition in some languages/cases)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+				-- Go To Implementation: Jumps to implementation(s) of an interface/method
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+				-- Go To References: Lists all references to the symbol
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+				-- Hover: Shows type information and docstrings (Pyright provides this)
 				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>h", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+				-- Rename: Renames the symbol across the project/scope
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+				-- Code Actions: Shows available actions (like Organize Imports from Ruff)
+				vim.api.nvim_buf_set_set(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+				vim.api.nvim_buf_set_set(bufnr, "v", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts) -- Also in visual mode
+				-- Diagnostics mappings (helpful for seeing errors/warnings)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "[j", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 				if client.name == "ruff" then
 					client.server_capabilities.hoverProvider = false
 					vim.api.nvim_create_autocmd("BufWritePre", {
