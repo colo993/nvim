5678208e4a947f0d8b37f88491889c52ae1c878b
colo993 <wakula993@gmail.com>
2025-07-12 17:27:16 +0200

    add extra kanagawa shortcut theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,8 +5,9 @@ return {
     name = "kanagawa",
     priority = 1000,
     config = function()
+      vim.keymap.set("n", "<leader>kw", ":colorscheme kanagawa-wave<CR>", { desc = "Kanagawa Wave theme" })
       vim.keymap.set("n", "<leader>kd", ":colorscheme kanagawa-dragon<CR>", { desc = "Kanagawa Dragon theme" })
-      vim.keymap.set("n", "<leader>kl", ":colorscheme kanagawa-lotus<CR>", { desc = "Kanagawa Lotus theme" })  
+      vim.keymap.set("n", "<leader>kl", ":colorscheme kanagawa-lotus<CR>", { desc = "Kanagawa Lotus theme" })
 
       local kanagawa = require("kanagawa")
 
